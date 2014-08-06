df134891718c8e6e33af42562b1257fe74d0eb0e
Alex Suykov <alex.suykov@gmail.com>
2014-08-06 14:36:48 +0300

    transferpids test

---
+++ test/t_transferpids.c
@@ -0,0 +1,54 @@
+#include "test.h"
+#include "../init.h"
+#include "../init_conf.h"
+
+#define R0 (1<<0)
+#define R1 (1<<1)
+#define R2 (1<<2)
+#define R3 (1<<3)
+#define R01 (R0 | R1)
+
+int currlevel = R2;
+int state = 0;
+struct initrec** inittab = NULL;
+
+struct initrec c0 = { .name = "c0", .flags = C_ONCE, .rlvl = R1,  .pid = 0 };
+struct initrec c1 = { .name = "c1", .flags = C_ONCE, .rlvl = R2, .pid = -1 };
+struct initrec c2 = { .name = "", .flags = C_ONCE, .rlvl = R2,  .pid = -1 };
+struct initrec c3 = { .name = "c3", .flags = 0, .rlvl = R2, .pid = 4 };
+struct initrec c4 = { .name = "c4", .flags = 0, .rlvl = R2, .pid = 5 };
+
+struct initrec n0 = { .name = "c0", .flags = C_ONCE, .rlvl = R1,  .pid = 0 };
+struct initrec n1 = { .name = "c1", .flags = C_ONCE, .rlvl = R2, .pid = -1 };
+struct initrec n2 = { .name = "", .flags = C_ONCE, .rlvl = R2,  .pid = 0 };
+struct initrec n3 = { .name = "c3", .flags = 0, .rlvl = R2,  .pid = 0 };
+struct initrec n5 = { .name = "c5", .flags = 0, .rlvl = R2,  .pid = 0 };
+
+struct initrec* cfg_inittab[] = { &c0, &c1, &c2, &c3, &c4, NULL };
+struct initrec* ncf_inittab[] = { &n0, &n1, &n2, &n3, &n5, NULL };
+
+struct config cfg_struct = { .inittab = cfg_inittab };
+struct config ncf_struct = { .inittab = ncf_inittab };
+
+struct config* cfg = &cfg_struct;
+struct config* ncf = &ncf_struct;
+
+struct memblock newblock = { .addr = &ncf_struct };
+
+extern void transferpids(void);
+
+void readinittab(void) { };
+void addptrsarray(void) { };
+
+int main(void)
+{
+	transferpids();
+
+	A(n0.pid == 0);		/* unrelated C_ONCE */
+	A(n1.pid == -1);	/* effectively transferred from the old entry */
+	A(n2.pid == -1);	/* current RL rule */
+	A(n3.pid == 4);		/* named service has been transferred */
+	A(n5.pid == 0);		/* new service, no pid change */
+
+	return 0;
+}
