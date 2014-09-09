2130d8ab2dac93ad6cc09c4190a1f03b572d4f8d
Alex Suykov <alex.suykov@gmail.com>
2014-09-09 05:23:42 +0300

    asm syscalls cleanup

--- libc/x86_64/syscalls.h
+++ libc/x86_64/syscalls.h
@@ -1,32 +1,5 @@
 #include <bits/syscall.h>
 
-#ifdef __PIC__
-#define syscall_weak(name,wsym,sym) \
-.text; \
-.type wsym,@function; \
-.weak wsym; \
-wsym: ; \
-.type sym,@function; \
-.global sym; \
-sym: \
-	mov	$__NR_##name,%al; \
-	jmp	__unified_syscall@PLT
-
-#define syscall(name,sym) \
-.text; \
-.type sym,@function; \
-.global sym; \
-sym: \
-.ifge __NR_##name-256 ; \
-	mov	$__NR_##name,%ax; \
-	jmp	__unified_syscall_16bit@PLT;  \
-.else ; \
-	mov	$__NR_##name,%al; \
-	jmp	__unified_syscall@PLT;  \
-.endif
-
-#else
-
 #define syscall_weak(name,wsym,sym) \
 .text; \
 .type wsym,@function; \
@@ -37,10 +10,10 @@ wsym: ; \
 sym: \
 .ifge __NR_##name-256 ; \
 	mov	$__NR_##name,%ax; \
-	jmp	__unified_syscall_16bit;  \
+	jmp	unisysx;  \
 .else ; \
 	mov	$__NR_##name,%al; \
-	jmp	__unified_syscall;  \
+	jmp	unisys;  \
 .endif
 
 #define syscall(name,sym) \
@@ -50,9 +23,8 @@ sym: \
 sym: \
 .ifge __NR_##name-256 ; \
 	mov	$__NR_##name,%ax; \
-	jmp	__unified_syscall_16bit; \
+	jmp	unisysx; \
 .else ; \
 	mov	$__NR_##name,%al; \
-	jmp	__unified_syscall; \
+	jmp	unisys; \
 .endif
-#endif
