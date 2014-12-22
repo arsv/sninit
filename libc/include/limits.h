5d8c8bac20be91a4dcf1aef7d3e4cfd72266a580
Alex Suykov <alex.suykov@gmail.com>
2014-12-22 16:02:13 +0200

    add __WORDSIZE to limits.h
    A single test, x_printf, needs it.

--- libc/include/limits.h
+++ libc/include/limits.h
@@ -1,6 +1,8 @@
 #ifndef LIMITS_H
 #define LIMITS_H
 
+#include <bits/limits.h>
+
 #define PATH_MAX	4095	/* # chars in a path name */
 
 #endif
---
+++ libc/x86_32/bits/limits.h
@@ -0,0 +1,6 @@
+#ifndef BITS_LIMITS_H
+#define BITS_LIMITS_H
+
+#define __WORDSIZE 32
+
+#endif
