24b3af6bfe2ca5873918d468720b41763b60e4f3
Alex Suykov <alex.suykov@gmail.com>
2014-12-22 17:12:33 +0200

    test printf use proper values for 32/64 arches

--- test/x_printf.c
+++ test/x_printf.c
@@ -1,14 +1,22 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 
 int main(int argc, char** argv)
 {
 	char s[] = "foobar";
 	int i = 1247;
-	long l = 1234567890;
 	unsigned x = 0x123ABC;
-	unsigned long lx = 0x123456789A;
+#if __WORDSIZE == 64
+	long l = 1234567890;
+	unsigned long lx = 0x123456789ABCDE;
+	void* ptr = (void*)0x1122334455667788;
+#else
+	long l = 12345678;
+	unsigned long lx = 0x12345678;
+	void* ptr = (void*)0x11223344;
+#endif
 
 	printf("non-format string\n"); /* this is going to be puts(), thanks gcc */
 	printf("str: %s\n", s);
@@ -16,7 +24,7 @@ int main(int argc, char** argv)
 	printf("long: %li\n", l);
 	printf("hex: 0x%08X\n", x);
 	printf("long hex: 0x%lX\n", lx);
-	printf("pointer: 0x%p\n", argv);
+	printf("pointer: 0x%p\n", ptr);
 
 	open("nonexistant", O_RDONLY);
 	printf("error: %m\n");
