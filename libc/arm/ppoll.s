0ea218e2657a9c6880f738860445674a3ecc18d9
Alex Suykov <alex.suykov@gmail.com>
2014-12-26 23:17:19 +0200

    arm: reboot() and ppoll() need extra arguments

--- libc/arm/ppoll.s
+++ libc/arm/ppoll.s
@@ -1,11 +1,16 @@
-# auto-generated, remove this line before editing
 .equ NR_ppoll, 336
+.equ sizeof_sigset_t, 8
 
 .text
+.align 4
 .global ppoll
+
+@ ppoll has 5th argument not listed in the prototype sizeof(sigset_t).
+@ An extra .c file would have solved the problem of course, but why bother?
 
 ppoll:
 	stmfd	sp!,{r4,r5,r7,lr}
+	ldr	r4, =sizeof_sigset_t
 	ldr	r7, =NR_ppoll
 	b	unisys
 
--- libc/arm/reboot.s
+++ libc/arm/reboot.s
@@ -1,11 +1,15 @@
-# auto-generated, remove this line before editing
 .equ NR_reboot, 88
+.equ LINUX_REBOOT_MAGIC1, 0xfee1dead
+.equ LINUX_REBOOT_MAGIC2, 672274793
 
 .text
 .global reboot
 
 reboot:
 	stmfd	sp!,{r4,r5,r7,lr}
+	mov	r2, r0 @ cmd
+	ldr	r0, =LINUX_REBOOT_MAGIC1
+	ldr	r1, =LINUX_REBOOT_MAGIC2
 	ldr	r7, =NR_reboot
 	b	unisys
 
