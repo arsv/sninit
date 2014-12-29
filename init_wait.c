62eb31028d642861c867f2b2a352d7c215ace612
Alex Suykov <alex.suykov@gmail.com>
2014-12-29 15:56:07 +0200

    report abnormal termination only

--- init_wait.c
+++ init_wait.c
@@ -11,15 +11,26 @@ void waitpids(void)
 	pid_t pid;
 	int status;
 	struct initrec *p, **pp;
+	int v;
 
 	while((pid = waitpid(-1, &status, WNOHANG)) > 0) {
 		for(pp = cfg->inittab; (p = *pp); pp++) {
 			if(p->pid != pid)
 				continue;
-			if(!(p->flags & C_WAIT))
-				warn("%s[%i] died (%i)", p->name, p->pid, status);
-			else if(status)
-				warn("%s[%i] abnormal exit (%i)", p->name, p->pid, status);
+
+			if(WIFSTOPPED(status)) {
+				/* XXX: maybe set P_SIGSTOP here?.. */
+				continue;
+			} else if(WIFEXITED(status)) {
+				if((v = WEXITSTATUS(status)))
+					warn("%s[%i] abnormal exit (%i)", p->name, p->pid, v);
+			} else if(WIFSIGNALED(status)) {
+				if((v = WTERMSIG(status)) && !(p->flags & P_SIGTERM))
+					warn("%s[%i] killed by signal %i", p->name, p->pid, WTERMSIG(status));
+				/* well, it could have been some other signal, but hey,
+				   if init was trying to kill it anyway, who cares why it died */
+			}
+
 			p->pid = -1;
 			p->flags &= ~(P_SIGTERM | P_SIGKILL | P_ZOMBIE);
 		}
--- libc/include/sys/wait.h
+++ libc/include/sys/wait.h
@@ -5,6 +5,13 @@
 
 #define WNOHANG		0x00000001
 
+#define WEXITSTATUS(status)	(((status) & 0xff00) >> 8)
+#define WTERMSIG(status)	((status) & 0x7f)
+
+#define WIFEXITED(status)	(WTERMSIG(status) == 0)
+#define WIFSIGNALED(status)	(!WIFSTOPPED(status) && !WIFEXITED(status))
+#define WIFSTOPPED(status)	(((status) & 0xff) == 0x7f)
+
 pid_t waitpid(pid_t pid, int *status, int options);
 
 #endif
