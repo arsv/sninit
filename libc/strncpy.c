8b7c49c281b49fbc230bcfc3d525a9ab50e8f0cd
Alex Suykov <alex.suykov@gmail.com>
2015-01-24 01:48:32 +0200

    libc: strncpy cleanup

--- libc/strncpy.c
+++ libc/strncpy.c
@@ -1,11 +1,13 @@
-#define _POSIX_SOURCE
-#define _XOPEN_SOURCE
-#include <sys/types.h>
 #include <string.h>
 
-/* gcc is broken and has a non-SUSv2 compliant internal prototype.
- * This causes it to warn about a type mismatch here.  Ignore it. */
-char *strncpy(char *dest, const char *src, size_t n) {
-  memccpy(dest,src,0,n);
-  return dest;
+char *strncpy(char* d, const char* s, size_t n)
+{
+	const char* e = s + n;
+	char* ret = d;
+
+	while(*s && s < e)
+		*(d++) = *(s++);
+	*d = '\0';
+
+	return ret;
 }
