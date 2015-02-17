12a7618c20b3723068a58443ecb52b95673490b1
Alex Suykov <alex.suykov@gmail.com>
2015-02-17 22:33:11 +0200

    set default namelen to 16, BR has some rather long entries

--- config.h
+++ config.h
@@ -39,7 +39,7 @@
 #define CMDBUF 100
 
 /* initrec name length (SysVinit standard value is 4+1) */
-#define NAMELEN 8
+#define NAMELEN 16
 
 /* Memory allocation granularity, for mmap(2). Should be something close to OS page size */
 #define IRALLOC 4096
