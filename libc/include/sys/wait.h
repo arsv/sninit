08c43c6f053de736506ae070842861655513acb3
Alex Suykov <alex.suykov@gmail.com>
2015-05-25 22:55:58 +0300

    libc: WUNTRACED and WCONTINUED

--- libc/include/sys/wait.h
+++ libc/include/sys/wait.h
@@ -3,14 +3,17 @@
 
 #include <sys/types.h>
 
-#define WNOHANG		0x00000001
+#define	WNOHANG		1	/* Don't block waiting.  */
+#define WUNTRACED	2	/* Report stopped child. */
+#define WCONTINUED	8	/* Report continued child.  */
 
-#define WEXITSTATUS(status)	(((status) & 0xff00) >> 8)
-#define WTERMSIG(status)	((status) & 0x7f)
+#define WEXITSTATUS(status)	(((status) & 0xFF00) >> 8)
+#define WTERMSIG(status)	((status) & 0x7F)
 
 #define WIFEXITED(status)	(WTERMSIG(status) == 0)
 #define WIFSIGNALED(status)	(!WIFSTOPPED(status) && !WIFEXITED(status))
-#define WIFSTOPPED(status)	(((status) & 0xff) == 0x7f)
+#define WIFSTOPPED(status)	(((status) & 0xFF) == 0x7F)
+#define WIFCONTINUED(status)	((status) == 0xFFFF)
 
 pid_t waitpid(pid_t pid, int *status, int options);
 
