d9b6d78ee79c31e5815228a5c1f8920bdb8201dd
Alex Suykov <alex.suykov@gmail.com>
2015-09-09 00:48:22 +0300

    libc: rework/reformat sigaddset

--- libc/sigaddset.c
+++ libc/sigaddset.c
@@ -1,17 +1,15 @@
 #include <signal.h>
-#include <errno.h>
 
-#define __sigmask(sig)		( ((unsigned long)1) << (((sig)-1) % (8*sizeof(unsigned long))) )
-#define __sigword(sig)		( ((sig)-1) / (8*sizeof(unsigned long)) )
+int sigaddset(sigset_t *set, int sig)
+{
+	if(sig < 1 || sig > SIGRTMAX)
+		return -1;
+	else
+		sig--;
 
-int sigaddset(sigset_t *set, int signo) {
-  if ((signo<1)||(signo>SIGRTMAX)) {
-    errno=EINVAL;
-    return -1;
-  } else {
-    unsigned long __mask = __sigmask (signo);
-    unsigned long __word = __sigword (signo);
-    set->sig[__word]|=__mask;
-    return 0;
-  }
+	const int bpl = 8*sizeof(long); /* bits per long */
+
+	set->sig[sig/bpl] |= (1U << (sig % bpl));
+
+	return 0;
 }
