371285dfaeac56efea2976113382600693d83835
Alex Suykov <alex.suykov@gmail.com>
2015-09-16 21:25:44 +0300

    libc: mips ppoll proper sizeof(sigset_t) placement

--- libc/mips/ppoll.s
+++ libc/mips/ppoll.s
@@ -10,11 +10,14 @@
 .global ppoll
 .ent ppoll
 
+/* XXX: do we need to move sp here?
+   Or is it still that "caller-allocated" area we can use at will? */
+
 ppoll:
 	li	$v0, NR_ppoll
 	addi	$sp, $sp, -4
 	li	$t9, sizeof_sigset_t
-	sw	$t9, 0($sp)
+	sw	$t9, 16($sp)
 	syscall
 	addi	$sp, $sp, 4
 	la	$t9, unisys
