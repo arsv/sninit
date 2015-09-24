f63bc31390bd16496b09570d81646c745c903717
Alex Suykov <alex.suykov@gmail.com>
2015-09-24 18:22:55 +0300

    test dump routines

---
+++ test/t_dumpstate.c
@@ -0,0 +1,99 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../config.h"
+#include "../init.h"
+#include "test.h"
+
+int currlevel = (1<<2);
+int nextlevel = (1<<3);
+
+struct initrec rec1 = { .name = "first",        .pid = 123,    .flags = 0,      };
+struct initrec rec2 = { .name = "longrec2",     .pid = 1245,   .flags = 0,      };
+struct initrec rec3 = { .name = "longhidden",   .pid = 123456, .flags = C_ONCE  };
+struct initrec rec4 = { .name = "runonce",      .pid = 4,      .flags = C_ONCE  };
+struct initrec rec5 = { .name = "last",         .pid = 2,      .flags = 0,      };
+
+struct initrec* testinittab[] = { NULL, &rec1, &rec2, &rec3, &rec4, &rec5, NULL };
+
+struct config testconfig = {
+	.initnum = 5,
+	.inittab = testinittab + 1,
+	.env = NULL
+};
+struct config* cfg = &testconfig;
+
+/* warn() logging -- there is only one warn() call */
+
+extern int vsnprintf(char* buf, size_t len, const char* fmt, va_list ap);
+
+#define BUFLEN 100
+char warnbuf[BUFLEN];
+
+int warn(const char* fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(warnbuf, BUFLEN, fmt, ap);
+	va_end(ap);
+	return 0;
+}
+
+/* dumprec() logging */
+
+#define RECLEN 20
+struct reclog {
+	char name[NAMELEN];
+	int nw;
+	int pw;
+} reclog[RECLEN];
+
+int recptr = 0;
+
+void dumprec(struct initrec* rec, int nw, int pw)
+{
+	if(recptr >= RECLEN) return;
+
+	strcpy(reclog[recptr].name, rec->name);
+	reclog[recptr].nw = nw;
+	reclog[recptr].pw = pw;
+
+	recptr++;
+}
+
+/* Simple level matching -- there's no submasks and no C_INVERT code here,
+   and bringing init_pass.c in complicates linking a lot. */
+
+int shouldbeshown(struct initrec* p)
+{
+	return !(p->flags & C_ONCE);
+}
+
+/* Finally the test code. There is little point in testing branches within
+   dumpstate() as they are trivial. Instead, the point is to check dumprec
+   call sequence for a given set of initrecs. */
+
+extern void dumpstate(void);
+
+#define Eqi(val, exp) Eq(val, exp, "%i")
+
+#define TEST(i, tnm, tnw, tpw) { \
+	S(reclog[i].name, tnm); \
+	A(reclog[i].nw == tnw); \
+	A(reclog[i].pw == tpw); \
+}
+
+int main(void)
+{
+	dumpstate();
+
+	S(warnbuf, "Switching 2 to 3");
+	Eqi(recptr, 3);
+	TEST(0, "first",    8, 4);
+	TEST(1, "longrec2", 8, 4);
+	TEST(2, "last",     8, 4);
+
+	return 0;
+}
+
+NOCALL(levelmatch);
---
+++ test/t_shouldbeshown.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../init.h"
+
+int currlevel;
+int nextlevel;
+struct config* cfg = NULL;
+
+extern int shouldbeshown(struct initrec* p);
+
+#define SHOW(fl, rl) {\
+	struct initrec ir = { .flags = fl, .rlvl = rl, .pid = 0 };\
+	if(shouldbeshown(&ir))\
+		printf("%s:%i: OK showing %s %s\n", __FILE__, __LINE__, #fl, #rl);\
+	else\
+		printf("%s:%i: FAIL not showing %s %s\n", __FILE__, __LINE__, #fl, #rl);\
+}
+
+#define HIDE(fl, rl) {\
+	struct initrec ir = { .flags = fl, .rlvl = rl, .pid = 0 };\
+	if(shouldbeshown(&ir))\
+		printf("%s:%i: FAIL showing %s %s\n", __FILE__, __LINE__, #fl, #rl);\
+	else\
+		printf("%s:%i: OK not showing %s %s\n", __FILE__, __LINE__, #fl, #rl);\
+}
+
+#define R1 (1<<1)
+#define R2 (1<<2)
+#define R3 (1<<3)
+#define R4 (1<<4)
+#define Ra (1<<0xa)
+
+int levelmatch(struct initrec* p, int level)
+{
+	return (p->rlvl & level);
+}
+
+int warn(const char* fmt, ...)
+{
+	return 0;
+}
+
+int main(void)
+{
+	currlevel = R3;
+	nextlevel = R3;
+
+	SHOW(C_DOF,  R3 | R4);
+	HIDE(C_DOF,  R2 | R1);
+	HIDE(C_ONCE, R3);
+
+	currlevel = 1<<2;
+	nextlevel = 1<<3;
+
+	SHOW(C_DOF,  R3 | R4);
+	SHOW(C_DOF,  R2);
+	HIDE(C_DOF,  R4);
+	SHOW(C_ONCE, R3);
+
+	return 0;
+}
