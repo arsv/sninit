fd0c5dc02b2ab68ba54ac876b1972f2b8521f05c
Alex Suykov <alex.suykov@gmail.com>
2015-09-26 15:53:19 +0300

    buildroot: use local source tree for sninit

--- misc/buildroot/package/sninit/sninit.mk
+++ misc/buildroot/package/sninit/sninit.mk
@@ -5,15 +5,9 @@
 ################################################################################
 
 SNINIT_VERSION = HEAD
-SNINIT_SITE = $(call github,arsv,sninit,$(SNINIT_VERSION))
+SNINIT_SOURCE_DIR = $(realpath $(BR2_EXTERNAL)/../..)
 SNINIT_LICENSE = GPLv3
 SNINIT_LICENSE_FILES = COPYING
-
-# sninit can be built with autotools-package, but the most complex
-# part, SNINIT_CONFIGURE_CMDS, still needs changes to build it right
-# (like setting prefix=/ instead of /usr), and the remaining
-# SNINIT_*_CMDS are trivial.
-# So to keep things together, generic-package is used instead.
 
 SNINIT_CONF_OPTS = inittab=/etc/inittab initdir=/etc/rc
 
@@ -41,6 +35,10 @@ ifneq ($(BR2_PACKAGE_SNINIT_BUILTIN_INITTAB),"")
 SNINIT_CONF_OPTS += builtin=$(BR2_PACKAGE_SNINIT_BUILTIN_INITTAB)
 endif
 
+define SNINIT_EXTRACT_CMDS
+	git archive --remote=$(SNINIT_SOURCE_DIR) $(SNINIT_VERSION) | tar -x -C $(@D)
+endef
+
 define SNINIT_CONFIGURE_CMDS
 	(cd $(SNINIT_SRCDIR) && ./configure \
 		cc="$(TARGET_CC)" \
@@ -59,3 +57,7 @@ define SNINIT_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
+
+# Buildroot will try to download the files even if it does not make sense.
+# Suppress that by resetting $(2)_ALL_DOWNLOADS from pkg-generic.mk
+SNINIT_ALL_DOWNLOADS =
