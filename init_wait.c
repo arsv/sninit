d785a971270721837795d71bec1eda455bc8d938
Alex Suykov <alex.suykov@gmail.com>
2015-09-27 04:00:09 +0300

    rework DTF logic, allow SIGINT/SIGTERM

--- init_wait.c
+++ init_wait.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 #include "init.h"
 #include "scope.h"
 #include "sys.h"
@@ -12,9 +13,8 @@ extern time_t passtime;
 export void waitpids(void);
 
 local void markdead(struct initrec* p, int status);
-
 local void checkfailacts(struct initrec* p, int failed);
-local void faildisable(struct initrec* p);
+local int badsignal(int sig);
 
 /* So we were signalled SIGCHLD and got to check what's up with
    the children. And because we care about killing paused (as in SIGSTOP)
@@ -47,36 +47,52 @@ void waitpids(void)
 	state &= ~S_SIGCHLD;
 }
 
-/* For dead children, there's a difference between "exited normally"
-   and "exited abnormally", so we've got to decide that and, if configured,
-   take action upon abnormal exit.
-
-   The actual criteria depend on DOF/DTF flags, and may be non-zero
-   return code, or too short lifetime, or both. */
+/* Abnormal exits should be reported, and in case of DTF/DOF taken
+   actions upon. It is not a simple 0-return check however: applying
+   DOF/DTF to entries likely killed by the user is not a good idea,
+   and entries killed by init should not be reported. */
 
 void markdead(struct initrec* p, int status)
 {
-	int failed;
+	if(WIFEXITED(status) && !WEXITSTATUS(status))
+		;    /* zero exits are ok */
+	else if(WIFSIGNALED(status) && (p->flags & (P_SIGTERM | P_SIGKILL)))
+		;    /* expected exit, init killed it */
+	else if(p->flags & C_HUSH)
+		;    /* should not be reported */
+	else warn("%s[%i] abnormal exit %i", p->name, p->pid,
+		WIFEXITED(status) ? WEXITSTATUS(status) : -WTERMSIG(status));
 
-	if(p->flags & (P_SIGTERM | P_SIGKILL))
-		failed = 0; /* requested exit is always correct */
-	else
-		failed = (!WIFEXITED(status) || WEXITSTATUS(status));
+	if(p->flags & C_ONCE)
+		;  /* no point in timing run-once entries */
+	else if(p->flags & (C_DOF | C_DTF))
+		checkfailacts(p, status);
 
-	if(failed && !(p->flags & C_HUSH))
-		warn("%s[%i] abnormal exit %i", p->name, p->pid,
-			WIFEXITED(status) ? WEXITSTATUS(status) : -WTERMSIG(status));
-
-	if(p->flags & (C_DOF | C_DTF))
-		checkfailacts(p, failed);
-
-	/* mark the entry as safely dead */
 	p->pid = -1;
-	p->flags &= ~(P_SIGTERM | P_SIGKILL);
+	p->flags &= ~(P_SIGTERM | P_SIGKILL | P_SIGSTOP);
 }
 
-void checkfailacts(struct initrec* p, int failed)
+/* Termination by certain signals is not considered a failure,
+   because it is nearly always the user who sends the signal. */
+
+int badsignal(int sig)
 {
+	switch(sig) {
+		case SIGINT: return 0;
+		case SIGTERM: return 0;
+		default: return 1;
+	}
+}
+
+void checkfailacts(struct initrec* p, int status)
+{
+	int failed;
+
+	if(WIFSIGNALED(status))
+		failed = badsignal(WTERMSIG(status));
+	else
+		failed = WEXITSTATUS(status);
+
 	if(p->flags & C_DTF) {
 
 		int mintime = (p->flags & C_FAST ? TIME_TO_RESTART : MINIMUM_RUNTIME);
@@ -89,19 +105,17 @@ void checkfailacts(struct initrec* p, int failed)
 		else
 			failed = toofast;
 
-		if(failed && (p->flags & P_WAS_OK))
-			p->flags &= ~P_WAS_OK;
-		else if(failed)
-			faildisable(p);
-		else
+		if(!failed) {
 			p->flags |= P_WAS_OK;
+			return;
+		} else if(p->flags & P_WAS_OK) {
+			p->flags &= ~P_WAS_OK;
+			return;
+		}
+	};
 
-	} else if(failed && p->flags & C_DOF)
-		faildisable(p);
-}
-
-void faildisable(struct initrec* p)
-{
-	warn("%s[%i] failed, disabling", p->name, p->pid);
-	p->flags |= P_FAILED;
+	if(failed) {
+		warn("%s[%i] failed, disabling", p->name, p->pid);
+		p->flags |= P_FAILED;
+	}
 }
