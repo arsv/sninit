70d2784a6531d3bbe0c5f98b038f3803d9f3b357
Alex Suykov <alex.suykov@gmail.com>
2015-10-09 22:54:48 +0300

    c80 formatting

--- libc/libtest/strerror.c
+++ libc/libtest/strerror.c
@@ -1,15 +1,16 @@
 extern const char* sys_errlist[];
 extern const int sys_nerr;
 
-/* Standard prototype for strerror is char*(int), and sys_errlist is const char*[].
-   clang knows it and complains if strerror is declared as const char*(int) as it should be,
-   despite the lack of const in the prototype being apparently a remnant of the days
-   when const wasn't even a keyword.
+/* Standard prototype for strerror is char*(int), and sys_errlist is
+   const char*[]. Clang knows it and complains if strerror is declared
+   as const char*(int) as it should be, despite the lack of const
+   in the prototype being apparently a remnant of the days when const
+   wasn't even a keyword.
 
    This is bad and clang should feel bad.
 
-   Honoring the fossils, we hereby declare char* strerror BUT we keep in mind it is in fact
-   const char* strerror. */
+   Honoring the fossils, we hereby declare char* strerror BUT we keep in mind
+   it is in fact const char* strerror. */
 
 char* strerror(int err)
 {
