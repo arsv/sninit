85ea0c0f8ed71ef0204c079c21bc2546c86ce217
Alex Suykov <alex.suykov@gmail.com>
2015-10-10 03:20:03 +0300

    buildroot aarch64: do not mount/umount root

--- misc/buildroot/board/sninit/post_build.sh
+++ misc/buildroot/board/sninit/post_build.sh
@@ -9,8 +9,13 @@ case "$arch" in
 		# MIPS has no fb console
 		sed -i -e '/^tty1/d' "$target/etc/inittab" ;;
 	aarch64)
-		# no fb console, and ttyAMA0 instead of ttyS0
-		sed -i -e '/^tty1/d' -e 's/ttyS0/ttyAMA0/' \
+		# no fb console, ttyAMA0 instead of ttyS0,
+		# and initramfs which should not be mounted/umounted
+		sed -i -e '/^tty1/d' \
+			-e 's/ttyS0/ttyAMA0/' \
+			-e '/remount,rw/d' \
+			-e '/mount -a/d' \
+			-e '/umount -a/d' \
 			"$target/etc/inittab" ;;
 	*)
 		# for arches with fb colsole, we leave ttyS0 for gdb
