54cf285ec706fb50eefb2a0d79a1e3c094954313
Alex Suykov <alex.suykov@gmail.com>
2015-10-20 00:49:25 +0300

    libc arm64: fork is clone(SIGCHLD, ...)

--- libc/arm64/fork.s
+++ libc/arm64/fork.s
@@ -1,15 +1,16 @@
 /* NR_fork is listed as deprecated on newer arches including arm64 */
 .equ NR_clone, 220
+.equ SIGCHLD, 17
 
 .text
 .global fork
 
 fork:
-	mov	x0, 0		/* flags */
+	mov	x0, SIGCHLD	/* flags */
 	mov	x1, 0		/* child stack */
 	mov	x2, 0		/* ptid */
-	mov	x3, 0		/* ctid */
-	mov	x4, 0		/* pt_regs */
+	mov	x3, 0		/* pt_regs */
+	mov	x4, 0		/* ctid */
 	mov	x8, NR_clone
 	b	_syscall
 
