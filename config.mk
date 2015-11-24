647fe9b09b1f2f759ffbe6886994c33978bcad8f
Alex Suykov <alex.suykov@gmail.com>
2015-11-25 01:47:22 +0200

    remove ts blocks from config.mk

--- config.mk
+++ config.mk
@@ -27,22 +27,17 @@ builtin :=
 # Needed for $(builtin) parse tool
 HOSTCC := gcc
 
-# Init block configuration: {conf|null} {tz|notz|libcts|nots} [sys|err] [dents,ppoll]
+# Init block configuration: {conf|null} [sys|err] [dents,ppoll]
 #
 # runtime configuration
 #	conf	configurable init (can/does read /etc/inittab)
 #	null	null configuration, only builtin inittab
-# timestamps for syslog
-#	tz	parse /etc/localtime with bundled routines
-#	notz	do parse /etc/localtime, timestamps in UTC
-#	nots	do not put timestamps at all, rely on syslogd to add them
-#	libcts	use libc strftime to make timestamps
 # bundled routines (read sys_* files to see what that means)
 #	sys	vsnprintf() and strerror()
 #	err	strerror() (implied by sys)
 #	dents	getdents64() (only needed when linking against glibc)
 #	ppoll	ppoll via syscall() (only needed with unpatched dietlibc)
-initblocks := conf sys nots dents
+initblocks := conf sys dents
 
 # Extra targets to build on "make all". Only used for sbin atm.
 extra :=
