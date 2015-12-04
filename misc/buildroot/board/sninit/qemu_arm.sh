6cdbda94045a895462e4862a136004f0997869cb
Alex Suykov <alex.suykov@gmail.com>
2015-12-04 14:52:26 +0200

    buildroot: use vexpress board instead of versatile
    Cortex-A9 instead of 926t, dtb, and it can power down properly.

--- misc/buildroot/board/sninit/qemu_arm.sh
+++ misc/buildroot/board/sninit/qemu_arm.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 qemu-system-arm \
-	-M versatilepb \
+	-M vexpress-a9 \
 	-kernel output/images/zImage \
-	-drive file=output/images/rootfs.ext2,if=scsi,format=raw \
-	-append "root=/dev/sda" \
+	-dtb output/images/vexpress-v2p-ca9.dtb \
+	-sd output/images/rootfs.ext2 \
+	-append "root=/dev/mmcblk0" \
 	-serial pty \
-	-net nic,model=rtl8139 \
+	-net nic \
 	-net user,id=eth0,hostfwd=tcp:127.0.0.1:1234-:1234
