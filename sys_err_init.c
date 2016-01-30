c7895fcb433183aa8e86e6171a74e9ab2c7d61fe
Alex Suykov <alex.suykov@gmail.com>
2016-01-30 03:35:25 +0200

    comments formatting

--- sys_err_init.c
+++ sys_err_init.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <errno.h>
 
-/* Reporting integer errno values is not nice, even in init, but linking libc sterror()
-   brings in the whole syserrlist. This is while init itself can only
-   produce a handful of different errno values.
+/* Reporting integer errno values is not nice, even in init, but linking
+   sterror() from libc brings in the whole syserrlist. This is while init
+   itself can only produce a handful of different errno values.
 
-   The idea is to choose the middle ground, providing readable messages for expected
-   errors and printing anything unexpected as a integer. Getting something unexpected
-   in init is already a sign of a big trouble, so making the user go on and use
-   strerror(3)/perror(1)/etc to get the message should not make things much worse.
+   The idea is to choose the middle ground, providing readable messages
+   for expected errors and printing anything unexpected as a integer.
+   Getting something unexpected in init is already a sign of a big trouble,
+   so making the user go on and use strerror(3)/perror(1)/etc to get
+   the message should not make things much worse.
 
-   Finally, I'm not a big fan of verbose messages like "No such file or directory",
+   Finally, I don't like verbose messages like "No such file or directory",
    especially when they aren't accompanied by respective error code.
    Most man pages only list symbolic constants, ENOENT in this case, with
-   a syscall-specific description, so searching for "No such file ..." is pointless.
-   Showing symbolic constants, on the other hand, makes it easy to read both errno(3)
-   and relevant section 2 pages
+   a syscall-specific description, so searching for "No such file ..."
+   is pointless. Showing symbolic constants, on the other hand, makes it
+   easy to read both errno(3) and relevant section 2 pages
    (when it's clear from the message which syscall failed, that is) */
 
 /* The whole symbol-or-int thing only works via sys_printf %m.
@@ -23,8 +24,8 @@
    nor musl printfs use strerror internally. */
 
 /* For a long time this has been a switch(err), but it turns out gcc generates
-   relatively large jump table here. The same does not seem to happen with telinit
-   and run errlists, strange. */
+   relatively large jump table here. The same does not seem to happen with
+   telinit and run errlists, strange. */
 
 struct errlist {
 	unsigned char err;
